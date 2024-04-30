328baf8d1cac26115161a791648dab4aae666d92
durgaprasadreddy katam <106094706+durgaprasadreddykatam@users.noreply.github.com>
2024-04-30 03:14:13 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,86 @@
-import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { Route } from 'react-router-dom';
 import Login from './pages/Login';
+import '../index.css'
+import Validator from './components/Validator';
+import ToBeDeleteted from './pages/ToBeDeleteted';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+// /* Optional CSS utils that can be commented out */
+// import '@ionic/react/css/padding.css';
+// import '@ionic/react/css/float-elements.css';
+// import '@ionic/react/css/text-alignment.css';
+// import '@ionic/react/css/text-transformation.css';
+// import '@ionic/react/css/flex-utils.css';
+// import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
 import Register from './pages/Register';
 import Home from './pages/Home';
-import Validator from './components/Validator';
 import Update from './pages/Update';
 import Viewintro from './pages/Viewintro';
 import Test from './pages/Test';
+import BluetoothDevices from './components/ BluetoothDevices';
 import PredictionHistory from './pages/PredictionHistory';
-import { defaultUserDetails } from './components/userTypes';
-import React from 'react';
+import ImageTest from './pages/ImageTest';
+import { UserDetails,defaultUserDetails } from './components/userTypes';
+
+
+setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-  <IonReactRouter>
-    <IonRouterOutlet>
-      <Route path="/">
-        <Login/>
-      </Route> 
-      <Route path="/login">
-        <Login/>
-      </Route>
-      <Route path="/Register">
-        <Register/>
-      </Route>
-      <Route  path="/home">
+    <IonReactRouter>
+      <IonRouterOutlet >
+        <Route exact path="/">
+          <Login/>
+        </Route> 
+        <Route exact path="/login">
+          <Login/>
+        </Route>
+        <Route exact path="/Register">
+          <Register/>
+        </Route>
+        <Route exact path="/home">
+          <Validator>
+            <Home decodedToken={defaultUserDetails}/>
+          </Validator>
+        </Route>
+        <Route exact path="/taketest">
+          <Validator>
+            <Test decodedToken={defaultUserDetails}/>
+          </Validator>
+        </Route>  
+        
+        
+        
+        <Route exact path="/history">
+          <Validator>
+            <PredictionHistory decodedToken={defaultUserDetails}/>
+          </Validator>
+        </Route> 
+        <Route exact path="/Account">
         <Validator>
-          <Home decodedToken={defaultUserDetails}/>
-        </Validator>
-      </Route>
-      <Route  path="/taketest">
+            <Update decodedToken={defaultUserDetails}/>
+          </Validator>
+        </Route>
+        <Route exact path="/viewintro">
         <Validator>
-          <Test decodedToken={defaultUserDetails}/>
-        </Validator>
-      </Route>   
-      
-      <Route  path="/history">
-        <Validator>
-          <PredictionHistory decodedToken={defaultUserDetails}/>
-        </Validator>
-      </Route> 
-      <Route  path="/Account">
-        <Validator>
-          <Update decodedToken={defaultUserDetails}/>
-        </Validator>
-      </Route>
-      <Route  path="/viewintro">
-        <Validator>
-          <Viewintro/>
-        </Validator>
-      </Route>
-    </IonRouterOutlet>
-  </IonReactRouter>
-</IonApp>
+            <Viewintro/>
+          </Validator>
+        </Route>
+      </IonRouterOutlet>
+    </IonReactRouter>
+  </IonApp>
 );
 
 export default App;
